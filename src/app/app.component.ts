33128c4b3d53f25f32cc4a5440b770ee9db2ae0f
Parinita <parinita@Parinitas-MacBook-Pro.local>
2024-02-13 19:17:11 +0530

    Header

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomePageComponent } from './home-page/home-page.component';
+{HomePageComponent}
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, HomePageComponent]
+})
+export class AppComponent {
+  title = 'Website';
+}
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [MatMenuModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+
+
+export class HomePageComponent {
+  
+
+}
